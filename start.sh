87f4d812ba9b18eabdff503397600f41f3c93dd3
BuildTools <unconfigured@null.spigotmc.org>
2021-04-06 17:03:07 +0900

    スタートスクリプトの修正

--- start.sh
+++ start.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-uvicorn app.main:app --reload --host 0.0.0.0 --port 8000
+pip3 install fastapi uvicorn aiohttp pylxd psutil
+ 
+/home/`whoami`/.local/bin/uvicorn app.main:app --reload --host 0.0.0.0 --port 8000
