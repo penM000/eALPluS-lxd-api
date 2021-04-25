e6c777ba76132cebd3f4fa4ca56d133ed73b8e34
BuildTools <unconfigured@null.spigotmc.org>
2021-04-25 15:21:25 +0900

    tagの追加

---
+++ app/internal/lxd/tag.py
@@ -0,0 +1,26 @@
+from typing import Dict
+import pylxd
+
+
+class instance_tag():
+    def __init__(self, instance):
+        self.instance: pylxd.models.instance.Instance = instance
+        self.tag = dict()
+        self.tag = self.get()
+
+    def get(self) -> Dict[str, str]:
+        for key in self.instance.config.keys():
+            if key.startswith("user."):
+                self.tag[key[5:]] = self.instance.config[key]
+        return self.tag
+
+    def save(self):
+        tag = dict()
+        for key in self.tag.keys():
+            tag[f"user.{key}"] = str(self.tag[key])
+        self.instance.config.update(tag)
+        try:
+            self.instance.save(wait=True)
+            return True
+        except BaseException:
+            return False
