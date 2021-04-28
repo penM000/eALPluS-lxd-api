7b735f06110d9e8f347d176ce56c8ff0864fb0bc
BuildTools <unconfigured@null.spigotmc.org>
2021-04-28 21:54:32 +0900

    修正

--- app/internal/lxd/launch.py
+++ app/internal/lxd/launch.py
@@ -15,7 +15,7 @@ async def launch_container_instance(
         instance_type: str = "container",
         role_id: str = "",
         class_id: str = "",
-        autostart:str = "0"):
+        autostart: str = "0"):
     """
     container
     virtual-machine
@@ -33,7 +33,7 @@ async def launch_container_instance(
             "limits.cpu": str(cpu),
             "limits.memory": str(memory),
             "security.idmap.isolated ": "true",
-            "security.nesting": "true"
+            "security.nesting": "true",
             "boot.autostart": str(autostart)
         },
         "devices": {
