27db26995168a99c02fd9f9e8e450b75a1afb6c6
BuildTools <unconfigured@null.spigotmc.org>
2021-05-13 20:07:04 +0900

    root起動に対応

--- start.sh
+++ start.sh
@@ -2,5 +2,8 @@
 # スクリプトの場所に移動
 cd `dirname $0`
 pip3 install fastapi uvicorn aiohttp pylxd psutil apscheduler Jinja2
- 
-~/.local/bin/uvicorn app.main:app --reload --host 0.0.0.0 --port 8000 --log-level debug --proxy-headers
+if [ "root" = `whoami` ]; then
+  uvicorn app.main:app --reload --host 0.0.0.0 --port 8000 --log-level debug --proxy-headers
+else
+  ~/.local/bin/uvicorn app.main:app --reload --host 0.0.0.0 --port 8000 --log-level debug --proxy-headers
+fi
