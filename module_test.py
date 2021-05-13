1493e11387de22d10405be66359f4bb07d5add4e
BuildTools <unconfigured@null.spigotmc.org>
2021-05-13 13:25:50 +0900

    テスト

--- module_test.py
+++ module_test.py
@@ -1,5 +1,5 @@
 from app.internal.lxd.cluster import check_cluster, get_cluster_status
-
+from app.internal.lxd.client import client
 if __name__ == "__main__":
     import asyncio
     loop = asyncio.get_event_loop()
@@ -7,3 +7,4 @@ if __name__ == "__main__":
     print(hoge)
     hoge = loop.run_until_complete(get_cluster_status())
     print(hoge)
+    print(client.instances.get("c1").location)
